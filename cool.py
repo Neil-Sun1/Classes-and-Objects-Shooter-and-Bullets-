eb9c1f485308e96f76c982c05eb1ef06cbbc45ed
Neil <you@example.com>
2026-04-29 16:34:03 -0500

    done

--- cool.py
+++ cool.py
@@ -8,7 +8,7 @@ def playing_area():
     pen = Turtle()
     pen.ht()
     pen.speed(0)
-    pen.color('teal')
+    pen.color(generate_color())
     pen.begin_fill()
     pen.goto(-240,240)
     pen.goto(240,240)
@@ -23,16 +23,18 @@ class Player(Turtle):
         self.ht()
         self.speed(0)
         self.color(color)
+        self.playercolor = color
         self.penup()
         self.goto(x,y)
         self.setheading(90)
         self.shape("turtle")
         self.bullets = []
         self.alive = True
+        self.health = 3
         self.st()
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
-        screen.onkey(self.fire(color), fire_key)
+        screen.onkey(self.fire, fire_key)
 
     def turn_left(self):
         self.left(10)
@@ -46,47 +48,56 @@ class Player(Turtle):
             self.setheading(180 - self.heading())
         if self.ycor() > 230 or self.ycor() < -230:
             self.setheading(-self.heading())
-    def fire(self,color):
-        self.bullets.append(Bullet(self,color))
+    def fire(self):
+        self.bullets.append(Bullet(self))
 class Bullet(Turtle):
-    def __init__(self,player,color):
+    def __init__(self,player):
         super().__init__()
         self.ht()
         self.pu()
         self.speed(0)
-        self.color(color)
+        self.player=player
+        self.color(player.playercolor)
         self.setheading(player.heading())
         self.goto(player.xcor(),player.ycor())
         self.st()
-        self.player = player
-    def move(self,Player):
+        self.move()
+    def move(self):
         self.forward(4)
         if self.xcor() > 230 or self.xcor() < -230:
             self.ht()
-            Player.bullet.remove(self)
+            self.player.bullets.remove(self)
         if self.ycor() > 230 or self.ycor() < -230:
             self.ht()
-            Player.bullet.remove(self)
+            self.player.bullets.remove(self)
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(520,520)
 # Key Binding. Connects key presses and mouse clicks with function calls
 screen.listen()
 
-screen.tracer(2)
 playing_area()
 
-p1 = Player(-100, 0, "red",screen, "d", "a","space")
-p2 = Player(100,0,"blue",screen, "Right","Left","8")
+
+p1 = Player(-100, 0, "red",screen, "d", "a","w")
+p2 = Player(100,0,"blue",screen, "Right","Left","Up")
 
 
 while p1.alive and p2.alive:
     p1.move()
     p2.move()
     for b in p1.bullets:
-        bullets.move(p1)
-
-    screen.update()
-
-
+        b.move()
+        if p2.distance(b) <20:
+            p2.health -=1
+            if p2.health ==0:
+                p2.ht()
+                p2.alive = False
+    for b in p2.bullets:
+        b.move()
+        if p1.distance(b) <20:
+            p1.health -=1
+            if p1.health ==0:
+                p1.ht()
+                p1.alive = False
 screen.exitonclick()
